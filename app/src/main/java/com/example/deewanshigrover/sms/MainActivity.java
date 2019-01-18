8725f13cd2ef9d02e4656dc68298a5b6e6ff6e4f
deewanshi <32430040+deewanshi@users.noreply.github.com>
2019-01-18 11:15:34 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/deewanshigrover/sms/MainActivity.java
+++ app/src/main/java/com/example/deewanshigrover/sms/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.deewanshigrover.sms;
 
 import android.Manifest;
 //import android.app.PendingIntent;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
@@ -23,69 +24,79 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
 
-    Button b1;
-    private final static int SEND_SMS_PERMISSION_REQ = 1;
-    String message;
-
-    // private Location currentLocation;
+    private Button b1;
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        b1 = findViewById(R.id.button);
+        b1 = (Button) findViewById(R.id.sms);
         b1.setEnabled(false);
-
-
-        if (checkPermission()) {
-            b1.setEnabled(true);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_PHONE_STATE, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET, Manifest.permission.SEND_SMS}, 1);
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_PHONE_STATE, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.SEND_SMS, Manifest.permission.INTERNET},
+                        1);
+                b1.setEnabled(true);
+            }
         } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, SEND_SMS_PERMISSION_REQ);
+            b1.setEnabled(true);
         }
-
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String s1 = "9711854779";
-                String s2 = "Help me";
+                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                LocationListener loc_listener = new LocationListener() {
 
-
-                SmsManager smsManager = SmsManager.getDefault();
-                StringBuffer smsBody = new StringBuffer();
-                smsBody.append(Uri.parse(showLocation()));
-
-                if (!TextUtils.isEmpty(s1) && !TextUtils.isEmpty(smsBody.toString()) && !TextUtils.isEmpty(s2)) {
-                    s2.concat(" ");
-                    s2.concat(smsBody.toString());
-
-                    if (checkPermission(Manifest.permission.SEND_SMS)) {
-
-                       smsManager.sendTextMessage(s1, null, s2, null, null);
-                       // smsManager.sendTextMessage(s1, null, s2,null,null);
-                    } else {
-                        Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                    public void onLocationChanged(Location l) {
+                        double lat, lon;
+                        String s1 = "8178923714";
+                        String s2 = "Help me";
+                        SmsManager smsManager = SmsManager.getDefault();
+                        final StringBuffer smsBody = new StringBuffer();
+                        lat = l.getLatitude();
+                        lon = l.getLongitude();
+                        smsBody.append("Help me" + '\n' + " https://maps.google.com/maps?saddr=" + lat + "," + lon);
+                        if (!TextUtils.isEmpty(s1) && !TextUtils.isEmpty(s2) && count != 1) {
+                            smsManager.sendTextMessage(s1, null, smsBody.toString(), null, null);
+                            count++;
+                        }
                     }
-                } else {
-                    Toast.makeText(MainActivity.this, "Permission denied", Toast.LENGTH_SHORT).show();
+
+                    public void onProviderEnabled(String p) {
+                    }
+
+                    public void onProviderDisabled(String p) {
+                        Toast.makeText(MainActivity.this, "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
+                    }
+
+                    public void onStatusChanged(String p, int status, Bundle extras) {
+                    }
+
+                };
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
                 }
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, loc_listener);
             }
         });
-
     }
-
-    private boolean checkPermission(String sendSms) {
-
-        int checkpermission = ContextCompat.checkSelfPermission(this, sendSms);
-        return checkpermission == PackageManager.PERMISSION_GRANTED;
-    }
-
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
-            case SEND_SMS_PERMISSION_REQ:
+            case 1:
                 if (grantResults.length > 0 && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                     b1.setEnabled(true);
                 }
@@ -93,61 +104,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private String showLocation() {
-
-        LocationManager locationManager = (LocationManager)
-                getSystemService(LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        String bestProvider = locationManager.getBestProvider(criteria, false);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return "";
-        }
-        Location location;
-        LocationListener loc_listener = new LocationListener() {
-
-            public void onLocationChanged(Location l) {
-            }
-
-            public void onProviderEnabled(String p) {
-            }
-
-            public void onProviderDisabled(String p) {
-            }
-
-            public void onStatusChanged(String p, int status, Bundle extras) {
-            }
-        };
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return "";
-        }
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, loc_listener);
-        location = locationManager.getLastKnownLocation(bestProvider);
-        double lat, lon;
-        try {
-            lat = location.getLatitude();
-            lon = location.getLongitude();
-            message="http://maps.google.com/maps?saddr="+lat+","+lon;
-        } catch (NullPointerException e) {
-            lat = -1.0;
-            lon = -1.0;
-        }
-        return message;
-    }
-
 }
-
